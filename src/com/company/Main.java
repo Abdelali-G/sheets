ee47a38702b3c1f4fd3cbe49f0ef85c6b7eedd64
AbdelaliAwad <Abdelali505710@eng.aswu.edu.eg>
2021-10-19 17:12:21 +0200

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //5.17
+        /* Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Enter the number of lines from\n" +
+                "1 to 15: ");
+        int number_of_lines = input.nextInt();
+
+        int y = 0;
+        int x = 0;
+        while(number_of_lines>=1){
+            x = number_of_lines;
+            do {
+
+
+                x = x-1 ;
+                y = Math.abs(x);
+                System.out.println(y);
+
+            }while (y != number_of_lines);
+
+
+
+            System.out.println(number_of_lines);
+            number_of_lines--;
+
+        }
+*/
+        //5.29
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        System.out.print("Enter the first day of the year note : sat =1 sun =2 and so on : ");
+        int startDay = input.nextInt();
+        /* note : sat =1 sun =2 and so on  */
+        
+
+
+
+
+
+
+        }
+
+        }
+
+
